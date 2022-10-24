58eb0e6ebf482dbc8b1b6a17b65e711f1e7c70e5
paulm <Sebastian.Koch@Student.HTW-Berlin.de>
2022-10-24 08:26:38 +0200

    readme ergänzt, erste Path Versuche

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # mywebtechproject
+
+
+## Mein Vorhaben:
+
+* to-do-Liste mit Drag-and-Drop Funktion
+* angelehnt an Trello
--- src/main/java/htw/berlin/webtech/demo/GreetingController.java
+++ src/main/java/htw/berlin/webtech/demo/GreetingController.java
@@ -3,6 +3,8 @@ package htw.berlin.webtech.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
     @Controller
@@ -12,6 +14,13 @@ import org.springframework.web.bind.annotation.RequestParam;
         public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
             model.addAttribute("name", name);
             return "greeting";
+        }
+
+        // funktioniert nicht, kein template?!
+        @RequestMapping("/answer/{name}/{age}")
+        public String pathParams(@PathVariable("name") String name,
+                                 @PathVariable("age") String age) {
+            return String.format("%s is %s years old", name, age);
         }
 
     }
