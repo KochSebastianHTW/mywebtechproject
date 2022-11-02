8da8b2217ce1475b8d454f89b50baa2bc230c0d4
paulm <Sebastian.Koch@Student.HTW-Berlin.de>
2022-11-02 14:07:40 +0100

    minor fix for test Mappings

--- src/main/java/htw/berlin/webtech/webDemo/CardRestController.java
+++ src/main/java/htw/berlin/webtech/webDemo/CardRestController.java
@@ -33,8 +33,8 @@ public class CardRestController {
                           @PathVariable("rank") String rank,
                           @PathVariable("name") String name) {
         return String.format(
-                "Einheit: %s" +
-                "Rang: %s" +
+                "Einheit: %s\n" +
+                "Rang: %s\n" +
                 "Name: %s",
                 unit, rank, name
         );
